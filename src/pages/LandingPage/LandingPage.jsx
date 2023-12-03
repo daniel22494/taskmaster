6f05ce3ef2e939dcd4569e8ca3366cd449c30d33
Daniel_Martinez <daniel.umanamartinez@gmail.com>
2023-12-03 11:02:13 -0500

    added some component scaffolding

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -2,7 +2,27 @@ import "./LandingPage.scss"
 
 function LandingPage() {
   return (
-    <div>LandingPage</div>
+    <>
+    <div className="header">
+        <h1 className="header__content">TaskMaster</h1>
+    </div>
+    <div className="tasks">
+        <button className="tasks__add">Add Task</button>
+        <div className="tasks_status">
+            <button>Done</button>
+            <button>Active</button>
+        </div>
+        <div className="tasks__list">
+            <div className="tasks__list-item">
+                <h3>Title</h3>
+                <p>Description</p>
+                <p>Due date</p>
+
+            </div>
+        </div>
+    </div>
+    </>
+    
   )
 }
 
